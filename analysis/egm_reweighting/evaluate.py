b2542d0181043297652c27e0bd3d59596eeb1bd7
jlangfor <jl2117@ic.ac.uk>
2024-10-31 17:22:35 +0000

    Fix evaluation script and validated to icenet output

--- analysis/egm_reweighting/evaluate.py
+++ analysis/egm_reweighting/evaluate.py
@@ -3,59 +3,100 @@ import numpy as np
 import pandas as pd
 import pickle as pkl
 import sys
+import os
 import re
 
+import argparse
+
+from utils import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input-mc', dest='input_mc', default='dy.parquet', help='Input dy to evaluate model on')
+parser.add_argument('-d', '--input-data', dest='input_data', default='data.parquet', help='Input data')
+parser.add_argument('--zscore-file', dest='zscore_file', default='zscore.pkl', help='Path to z-score file')
+parser.add_argument('--m1-path', dest='m1_path', default='.', help='Path to Model 1 files')
+parser.add_argument('--m2-path', dest='m2_path', default='.', help='Path to Model 2 files')
+parser.add_argument('-p','--parameters', dest="parameters", default="tau1=1,tau2=1,maxW=100", help="Evaluation parameter values")
+args = parser.parse_args()
+
 # Hyperparameters
-tau1 = 1.
-tau2 = 1.
-maxW = 100.
+params = {}
+for p in args.parameters.split(","):
+    params[p.split("=")[0]] = float(p.split("=")[1])
 
-# Input model files
-f_zscore = sys.argv[2]
-f_m1 = sys.argv[3]
-f_m2 = sys.argv[4]
+# Load events to evaluate
+print(f" --> Reading {args.input_mc}")
+events = pd.read_parquet(args.input_mc, engine="pyarrow")
 
-# Taken from ICENET: sigmoid(f/1-f) = exp
-def rw_transform_with_logits(logits, absMax=30):
-    logits = np.clip(logits, -absMax, absMax)
-    return np.exp(logits)
-
-# Load data events (replace with .parquet)
-# May need to batch load for many events?
-print(f" --> Reading {sys.argv[1]}")
-events = pd.read_csv(sys.argv[1])
-
-if "probe_pfChargedIso" not in events.columns:
-    events['probe_pfChargedIso'] = events['probe_pfChargedIsoPFPV']
-
-print(f" --> Evaluating model 1")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Load model 1 (3D)
-tau1 = 1
-model1 = xgboost.Booster()
-model1.load_model(f_m1)
-# No z-score normalisation for model1
-input_features = model1.feature_names
+print(f" --> Evaluating model 1")
+with open(args.m1_path, "rb") as fpkl:
+    m1 = pkl.load(fpkl)
+
+# Find best model
+best_idx = int(np.argmin(m1['losses']['val_losses']))
+best_model = m1['model'][:best_idx+1] 
+print(f"  * Using model at epoch: {best_idx}")
+
+# No Z-score normalisation for model 1
+input_features = best_model.feature_names
 X3D = xgboost.DMatrix(events[input_features])
 # Evaluate model
-logits1 = model1.predict(X3D)
-logits1 /= tau1
-events['rwgt_1'] = np.clip(rw_transform_with_logits(logits1), 0., 100.)
+logits1 = best_model.predict(X3D)
+logits1 /= params['tau1']
+events['rwgt_m1'] = np.clip(rw_transform_with_logits(logits1), 0., 100.)
 
-print(f" --> Evaluating model 2")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Load model 2
-model2 = xgboost.Booster()
-model2.load_model(f_m2)
+print(f" --> Evaluating model 2")
+with open(args.m2_path, "rb") as fpkl:
+    m2 = pkl.load(fpkl)
+
+# Find best model
+best_idx = int(np.argmin(m2['losses']['val_losses']))
+best_model = m2['model'][:best_idx+1]
+print(f"  * Using model at epoch: {best_idx}")
+
 # Apply z-score normalisation for model 2
-with open(f_zscore, "rb") as fpkl:
+with open(args.zscore_file, "rb") as fpkl:
     zscore = pkl.load(fpkl)
 input_features = zscore['ids']
 X = (events[input_features].to_numpy()-zscore['X_mu'])/zscore['X_std']
 X = xgboost.DMatrix(X, feature_names=input_features)
 # Evaluate modoel
-logits2 = model2.predict(X)
-logits2 /= tau2
-events['rwgt_2'] = np.clip(rw_transform_with_logits(logits2), 0., 100.)
+logits2 = best_model.predict(X)
+logits2 /= params['tau2']
+events['rwgt_m2'] = np.clip(rw_transform_with_logits(logits2), 0., 100.)
 
-f_out = re.sub(".csv", "_eval.csv", sys.argv[1].split("/")[-1])
-print(f" --> Saving as: samples_eval/{f_out}")
-events.to_csv(f"samples_eval/{f_out}")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Load data and add normalised weight columns
+print(f" --> Reading {args.input_data}")
+events_data = pd.read_parquet(args.input_data, engine="pyarrow")
+events_data['weight'] = 1
+
+# Add normalised weight for pre S1 plotting
+events['weight_norm'] = events['weight']*(events_data['weight'].sum()/events['weight'].sum())
+
+# Normalise MC yield to data after S1 and apply S2 weight
+events['w_post_S1'] = (events['weight']*events['rwgt_m1'])*(events_data['weight'].sum()/(events['weight']*events['rwgt_m1']).sum())
+events['w_post_S2_unnorm'] = events['w_post_S1']*events['rwgt_m2']
+events['w_post_S2'] = events['w_post_S2_unnorm']*(events['w_post_S1'].sum()/events['w_post_S2_unnorm'].sum())
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Concatenate dataframes and save
+print(" --> Saving processed dataframes")
+if not os.path.isdir("samples_processed"):
+    os.system("mkdir -p samples_processed")
+
+events['y'] = 0
+name = re.sub(".parquet", "_processed.parquet", args.input_mc.split("/")[-1])
+events.to_parquet(f"samples_processed/{name}", engine="pyarrow")
+
+events_data['y'] = 1
+name = re.sub(".parquet", "_processed.parquet", args.input_data.split("/")[-1])
+events_data.to_parquet(f"samples_processed/{name}", engine="pyarrow")
+
+
+
